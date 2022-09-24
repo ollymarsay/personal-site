34903cc1aba4f46e07e9801af70cd0835e5e68a5
ollymarsay <ojmarsay@gmail.com>
2022-09-24 17:21:03 +0100

    Update Coffee

--- components/HelloBar.tsx
+++ components/HelloBar.tsx
@@ -2,9 +2,9 @@ export default function HelloBar() {
     return (
       <a
         class="bg-red-400 text-black border(b red-500) p-3 text-center group"
-        href="https://www.origincoffee.co.uk/products/beija-flor"
+        href="https://www.origincoffee.co.uk/products/la-vanguardia?variant=40162592292937"
       >
-        <b>Brewing This Week:</b> Brazilian <b>Beija Flor</b> roasted by{" "}
+        <b>Brewing This Week:</b> Panama <b>La Vanguardia</b> roasted by{" "}
         <b>Origin Coffee</b>{"  "}<span class="group-hover:underline">→</span>
       </a>
     );
