553cc7b0d0d84917ce366fd98e3132422f6866f1
ollymarsay <ojmarsay@gmail.com>
2022-09-24 21:36:32 +0100

    ref in link

--- components/HelloBar.tsx
+++ components/HelloBar.tsx
@@ -2,7 +2,7 @@ export default function HelloBar() {
     return (
       <a
         class="bg-red-400 text-black border(b red-500) p-3 text-center group"
-        href="https://www.origincoffee.co.uk/products/la-vanguardia?variant=40162592292937"
+        href="https://www.origincoffee.co.uk/products/la-vanguardia?ll_ref_id=k5zDEaAuB&variant=40162592292937"
       >
         <b>Brewing This Week:</b> Panama <b>La Vanguardia</b> roasted by{" "}
         <b>Origin Coffee</b>{"  "}<span class="group-hover:underline">→</span>
